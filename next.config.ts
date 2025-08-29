bdde208d8dd400eb43b10dc2b7f21b6ae08bcf01
jojo3141 <118258268+jojo3141@users.noreply.github.com>
2025-08-29 10:00:45 +0200

    config changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // This tells Next.js to build static HTML for all pages
+  images: {
+    unoptimized: true, // required if you have <Image /> but no image loader
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
