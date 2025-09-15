3fcd2616bc1b27fd076abb53f34a1e303dbdea28
jojo3141 <118258268+jojo3141@users.noreply.github.com>
2025-09-15 08:38:04 +0200

    new base path

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // <- Hier aktivierst du den statischen Export
   // optional: weitere Konfigurationsoptionen hier
+  basePath: "/~jheger",
+  assetPrefix: "/~jheger/",
   
 };
 
